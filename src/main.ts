b42cd5a2c8083b00498049fcf130487682f0b222
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-16 21:52:41 +0300

    Fix home page, dashboard data and layouts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import ToastPlugin from 'vue-toast-notification';
+// Import one of the available themes
+import 'vue-toast-notification/dist/theme-default.css';
+//import 'vue-toast-notification/dist/theme-bootstrap.css';
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(ToastPlugin);
 app.use(router)
 
 app.mount('#app')
