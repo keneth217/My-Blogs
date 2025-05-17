6f95c6ca594796da1c7d4ece3b3dfed90fc583ff
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-17 22:49:08 +0300

    Add RichTextEditor component to format  blogs alignments

---
+++ src/views/icons/RedoIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+    <path d="M18.172 7l-2.536 2.536 1.414 1.414L22 6l-4.95-4.95-1.414 1.414L18.172 5H11a8 8 0 0 0 0 16h9v-2h-9a6 6 0 1 1 0-12h7.172z"/>
+  </svg>
+</template>
