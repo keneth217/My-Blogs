95bb28e93a6659f040d4283d00be7c799ec1b433
KENETH217 <kipyegonkeneth03@gmail.com>
2025-05-17 22:49:00 +0300

    Add RichTextEditor component to format  blogs alignments

---
+++ src/views/icons/OrderedListIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
+    <path d="M8 4h13v2H8V4zM5 3v3h1v1H3V6h1V4H3V3h2zm-2 7h3.5v1H3v-1zm0 7h3.5v1H3v-1zm0 2h3.5v1H3v-1zm5-5h13v2H8v-2zm0 7h13v2H8v-2z"/>
+  </svg>
+</template>
